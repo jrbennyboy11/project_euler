b98e7df0b14723b908242f156584c8c7af70b5ca
Ben Barnett <bbarnett@stevens.edu>
2015-03-13 04:39:36 -0400

    Completed problem 79

--- problem79.cpp
+++ problem79.cpp
@@ -10,11 +10,9 @@ int main()
 	ifstream successes;
 	unsigned int i;
 	unsigned int j;
-	unsigned int k;
 	unsigned int index;
 	unsigned int numsBefore[10];
 	unsigned int password[10];
-	unsigned int attempts[50][3];
 	bool numbers[10][10]; //numbers n 0-9 have numbers numbers[n][0-9] before them
 	
 	for(i=0;i<10;i++)
@@ -41,13 +39,10 @@ int main()
 		unsigned int in2val = in2-'0';
 		unsigned int in3val = in3-'0';
 		
+		//numbers[in1val][in2val] means inval1 is before inval2 in the password
 		numbers[in1val][in2val] = true;
 		numbers[in1val][in3val] = true;
 		numbers[in2val][in3val] = true;
-		
-		attempts[i][0] = in1val;
-		attempts[i][1] = in2val;
-		attempts[i][2] = in3val;
 	}
 	
 	successes.close();
@@ -57,23 +52,12 @@ int main()
 			if(numbers[i][j])
 				numsBefore[i]++;
 	
-	for(i=0;i<10;i++)
-	{	
-		if(numsBefore[i] == 0)
-		{
-			bool found = false;
-			
-			for(j=0;j<50;j++)
-				if(attempts[j][0] == i)
-					found = true;
-			
-			if(!found)
-				cout << i << " doesn't need to be in the password" << endl;
-		}
-	}
-	
 	index = 9;
 	
+	/*
+	 * Put the numbers in order from ones that are before
+	 * the most numbers to the ones that are before the least
+	 */
 	for(i=0;i<10;i++)
 		for(j=0;j<10;j++)
 			if(numsBefore[j] == i)
@@ -81,29 +65,23 @@ int main()
 	
 	cout << "Password: ";
 	for(i=0;i<10;i++)
-		cout << password[i];
-	cout << endl;
-	
-	//Password check
-	for(i=0;i<50;i++)
 	{
-		int index1;
-		int index2;
-		int index3;
-		
-		for(j=0;j<10;j++)
-			if(password[j] == attempts[i][0])
-				index1 = j;
-		
-		for(j=0;j<10;j++)
-			if(password[j] == attempts[i][1])
-				index2 = j;
-		
-		for(j=0;j<10;j++)
-			if(password[j] == attempts[i][2])
-				index3 = j;
-		
-		if(!((index1 < index2) && (index1 < index3) && (index2 < index3)))
-			cout << "Password Failed" << endl;
+		/*
+		 * If the number isn't before any other numbers
+		 * and no numbers are before it, then it doesn't
+		 * need to be in the password
+		 */
+		if(numsBefore[password[i]] == 0)
+		{
+			bool found = false;
+			for(j=0;j<50;j++)
+				if(numbers[i][password[i]])
+					found = true;
+			if(found)
+				cout << password[i];
+		}
+		else
+			cout << password[i];
 	}
+	cout << endl;
 }
