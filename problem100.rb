30ef20430058aaaeef666e3b99238938ebf8627d
Ben Barnett <jrbennyboy11@gmail.com>
2015-04-29 10:46:59 -0400

    revised problem 100

--- problem100.rb
+++ problem100.rb
@@ -1,28 +1,29 @@
-threads = (0..6).map do |i|
+threads = (0..4).map do |i|
   Thread.new(i) do |i|
 	answer = 0
-	blue = 0
-	red = 0
-	total = 1000000000000.0
-	i.times{total *= 5}
-	endpoint = total*5
+	t = 1000000000000.0	
+	i.times{t += 1000000000000.0}
+	endpoint = t+1000000000000.0
 
-	while answer == 0 && total < endpoint
-		total += 1
-		blue = (total*0.5).ceil
-
-		chance = (blue/total)*(blue-1/total-1)
-		while chance < 0.5
-			blue += 1
-			chance = (blue/total)*(blue-1/total-1)
+	while answer == 0 #1000000000000.0	
+		t += 1
+		b = 0.7*t
+		temp = t*(t-1)
+	
+		while 2*b*(b-1) < temp
+			b += 1
 		end
-		if chance == 0.5
-			answer = blue 
+		if 2*b*(b-1) == temp
+			answer = b
 		end
 	end
+
 	if answer > 0
-		puts "Answer: #{answer}"
-	end
-  end
+		puts "Number of blue discs: #{answer}"
+	else
+		puts "Answer is not between #{t} & #{endpoint}"
+  	end
 end
 threads.each {|t| t.join}
+
+#2b*(b-1) = t*(t-1)
