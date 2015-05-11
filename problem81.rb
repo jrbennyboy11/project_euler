7d0ec60dddfd9608ab32fbc0cca29bb2e4aec3ed
Ben Barnett <jrbennyboy11@gmail.com>
2015-05-11 15:19:08 -0400

    completed problem 81

--- problem81.rb
+++ problem81.rb
@@ -1,65 +1,58 @@
 def min(x,y)
-	if x == -1
-		return y
-	elsif y == -1
-		return x
-	elsif x > y
-		return y
+	if x < y
+		return x.to_i
 	else
-		return x
+		return y.to_i
 	end
 end
-
-#$i = 0
-
-$min_grid = Hash.new
-80.times do |i|
-	80.times do |j|
-		$min_grid[[i,j]] = -1
-	end
-end
-
-def min_path_sum(matrix, row, column, sum)
-	sum += matrix[row,column].to_s.to_i
-	
-	#$i += 1
-	#if $i%100000 == 0
-	#	puts "i: #{$i}"
-	#end
-
-	minVal = $min_grid.fetch(row,column)
-	if minVal != -1 && minVal < sum
-		return -1
-	else
-		$min_grid[[row,column]] = sum
-	end
-
-	if row == 79 || column == 79
-		79-row.times do |i|
-			sum += matrix[row+i,column].to_s.to_i
-		end
-		79-column.times do |i|
-			sum += matrix[row,column+i].to_s.to_i
-		end
-		return sum
-	else
-		return min(min_path_sum(matrix, row+1, column, sum) , min_path_sum(matrix, row, column+1, sum))
-	end
-end
-
-matrix = Array.new(80) { Array.new(80) }
-i = 0
-j = 0
  
+matrix = Array.new(80)
+i = 0
+
 matrixFile = File.open("p081_matrix.csv", "r")
-numbers = matrixFile.read
+	matrixFile.each_line do |line|
+		matrix[i] = Array.new(80)
+		temp = line.split(',')
+		j = 0
+		while j < 80
+			matrix[i][j] = temp[j].to_s.chomp.to_i
+			j += 1
+		end
+		i += 1
+	end
 matrixFile.close()
-numbersArr = numbers.split(',')
 
-numbersArr.each{|x|
-	matrix[i,j] = x
-	j = (j+1)%80
-	if j==79 then i += 1 end
-}
+i = 79 #row
+j = 79 #column
 
-puts "min path sum: #{min_path_sum(matrix,0,0,0)}"
+while i > 0
+	i -= 1
+	matrix[i][j] += matrix[i+1][j]
+end
+i = 79
+while j > 0
+	j -= 1
+	matrix[i][j] += matrix[i][j+1]
+end
+
+counter = 78
+
+while counter > 0
+	i = counter
+	j = counter
+	matrix[i][j] += min(matrix[i+1][j],matrix[i][j+1])
+
+	while i > 0
+		i -= 1
+		matrix[i][j] += min(matrix[i+1][j],matrix[i][j+1])
+	end
+	i = counter
+	while j > 0
+		j -= 1
+		matrix[i][j] += min(matrix[i+1][j],matrix[i][j+1])
+	end
+	
+	counter -= 1
+end
+
+puts "Solution: #{matrix[0][0] + min(matrix[0][1],matrix[1][0])}"
