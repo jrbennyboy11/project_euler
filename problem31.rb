62a41dbe9846a63355a79050d42920ef26184cac
Ben Barnett <jrbennyboy11@gmail.com>
2015-05-13 13:46:46 -0400

    completed problem 31

--- problem31.rb
+++ problem31.rb
@@ -1,59 +1,40 @@
-#73682
+a = 200 #200
+b = 0 #100
+c = 0 #50
+d = 0 #20
+e = 0 #10
+f = 0 #5
+g = 0 #2
 
-def num_ways(money, ival)
-	money_copy = Array.new(8)
-	money_copy = money
-	i = ival
-	count = 1
-	
-	while i<7
-		while money_copy[i] > 0
-			print money_copy
-			puts ""
-			if i == 0
-				money_copy[0] -= 1
-				money_copy[1] += 2
-				count += num_ways(money_copy,1)
-			end
-			if i == 1
-				money_copy[1] -= 1
-				money_copy[2] += 2
-				count += num_ways(money_copy,2)
-			end
-			if i == 2
-				money_copy[2] -= 1
-				money_copy[3] += 2
-				money_copy[4] += 1
-				count += num_ways(money_copy,3)
-			end
-			if i == 3
-				money_copy[3] -= 1
-				money_copy[4] += 2
-				count += num_ways(money_copy,4)
-			end
-			if i == 4
-				money_copy[4] -= 1
-				money_copy[5] += 2
-				count += num_ways(money_copy,5)
-			end
-			if i == 5
-				money_copy[5] -= 1
-				money_copy[6] += 2
-				money_copy[7] += 1
-				count += num_ways(money_copy,6)
-			end
-			if i == 6
-				money_copy[6] -= 1
-				money_copy[7] += 2
-				count += 1
+count = 0
+
+while a >= 0
+	b = a
+	a -= 200
+	while b >= 0
+		c = b
+		b -= 100
+		while c >= 0
+			d = c
+			c -= 50
+			while d >= 0
+				e = d
+				d -= 20
+				while e >= 0
+					f = e
+					e -= 10
+					while f >= 0
+						g = f
+						f -= 5
+						while g >= 0
+							g -= 2
+							count += 1
+						end
+					end
+				end
 			end
 		end
-		
-		i += 1 
 	end
-	
-	return count
 end
 
-money = [1,0,0,0,0,0,0,0] #200p, 100p, 50p, 20p, 10p, 5p, 2p, 1p
-puts "Num ways: #{num_ways(money,0)}"
+puts "Solution: #{count}"
