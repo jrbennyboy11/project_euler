f31683ac5b77253110a01ba37dd8268cb858d424
Ben Barnett <jrbennyboy11@gmail.com>
2015-06-12 06:58:23 -0400

    cleaned up the code and added comments

--- problem59.rb
+++ problem59.rb
@@ -1,71 +1,67 @@
-=begin
+# Benjamin Barnett
+# 6/12/15
+# https://projecteuler.net/problem=59
 
-Benjamin Barnett
-6/12/15
-https://projecteuler.net/problem=59
-
-=end
-
-def sum_of_ascii_valus(message,pass)
-	sum = 0
-	it = -1
-	pass_size = pass.length
-
-	decrypted_message = message.map{|c| c^pass[it = (it+1)%pass_size]}
-
-	decrypted_message.each do |c|
-		sum += c
-	end
-
-	return sum
+#message is an array of numbers, each representing an ASCII value
+#returns the sum of all the ASCII values in message
+def sum_of_ascii_values(message)
+	return message.map{|c| c.to_i}.reduce(:+)
 end
 
+#message is an array of numbers, each representing an ASCII value
+#returns whether or not the ASCII values in message fall within the range of normal text characters
+def is_valid_text(message)
+	return message.min >= 32 && message.max <= 122
+end
+
+#message is an array of numbers, each representing an ASCII value
+#returns string version of message with ASCII values as characters
+def message_to_string(message)
+	return message.map{|c| c.chr}.join('')
+end
+
+#message is an array of numbers, each representing an encrypted ASCII value
+#pass is an array of numbers, each representing an ASCII value, that will be cycled through and used as the decryption XOR for the encrypted values in message
+#returns an array of numbers, each representing an unencrypted ASCII value
 def decrypt_message(message, pass)
 	it = -1
-	pass_size = pass.length
-	
-	decrypted_message = message.map{|c| c^pass[it = (it+1)%pass_size]}
-	decrypted_message.each do |c|
-		if c < 32 || c > 122
-			return false
+	return message.map{|c| c^pass[it = (it+1)%pass.length]}
+end
+
+#messages is a hash table of decrypted messages as strings
+#returns the key of the message containing the most "the"s and "and"s
+def most_thes_ands(messages)
+	most = 0
+	most_key = 0
+
+	messages.each_pair do |key,val|
+		count = val.count(" the ") + val.count(" and ")
+		if count > most
+			most = count
+			most_key = key
 		end
 	end
-	
-	return decrypted_message.map{|c| c.chr}.join('')
+
+	return most_key
 end
 
 cipher_text = File.read("p059_cipher.txt").split(",").map(&:to_i)
 decrypted_messages = Hash.new
 
-i = 0
-while i < 26
-	#puts i
-	j = 0
-	while j < 26
-		k = 0
-		while k < 26
+26.times do |i|
+	26.times do |j|
+		26.times do |k|
 			pass = [97+i,97+j,97+k]
 			message = decrypt_message(cipher_text,pass)
-			if message != false
-				decrypted_messages[sum_of_ascii_valus(cipher_text,pass)] = message
+			if is_valid_text(message)
+				decrypted_messages[sum_of_ascii_values(message)] = message_to_string(message)
 			end
-			k += 1
 		end
-		j += 1
-	end
-	i += 1
-end
-
-real_message_key = -1
-the_count = 0
-
-decrypted_messages.each_pair do |key,val|
-	num_thes = val.count(" the ")
-	if num_thes > the_count
-		the_count = num_thes
-		real_message_key = key
 	end
 end
 
-puts "\n#{decrypted_messages[real_message_key]}"
-puts "\nSum of ASCII Values: #{real_message_key}\n\n"
+real_message_key = most_thes_ands(decrypted_messages)
+
+puts "\nDecrypted Message: \n\n*------------------*"
+puts "#{decrypted_messages[real_message_key]}\n*------------------*\n\n"
+puts "Sum of ASCII Values: #{real_message_key}\n\n"
