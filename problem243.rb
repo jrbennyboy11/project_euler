e922d7c11499a7f76dc5a0c8296dfa3c7b39afcd
Ben Barnett <jrbennyboy11@gmail.com>
2015-07-29 12:47:57 -0400

    wrote problem 243, have not gotten it to finish running yet...

--- problem243.rb
+++ problem243.rb
@@ -1,65 +1,20 @@
-def decimal_val(numerator,denominator,num_places)
-	n = numerator
-	d = denominator
-	dp = 0
-	retString = "0"
+resilience_below = Rational(15499,94744)
+current_resilience = Rational(1)
+i = 1
 
-	if n == d
-		return 1
-	elsif n>d
-		retString = (d/n).to_s
-		n = d%n
+while(current_resilience >= resilience_below)
+	if i%1000 == 0 then puts "i: #{i}" end
+
+	j = 1
+	i += 1
+	num_resilient = 0
+	
+	while(j < i)
+		if(Rational(j,i).denominator == i) then num_resilient += 1 end
+		j += 1
 	end
 	
-	retString += "."
-	
-	while dp < num_places
-		if d > n
-			n *= 10
-		end
-		
-		retString += (n/d).floor.to_s
-		n = n%d
-		
-		if n == 0
-			while retString.length < num_places+2
-				retString += "0"
-			end
-			
-			return retString
-		end
-			
-		dp += 1
-	end
-	
-	return retString
+	current_resilience = Rational(num_resilient,i-1)
 end
 
-below = decimal_val(15499,94744,50)
-
-n = 2 #numerator
-d = 10000 #denominator
-res = 0 #how many resiliants
-res_val = "0.9999" #resiliance value
-i = 0
-
-while below < res_val
-	d += 1
-	n = 2
-	res = 0
-	while n < d-1
-		if d%n != 0
-			res += 1
-		end
-		n += 1	
-	end
-	
-	res_val = decimal_val(res,d-1,50)
-	if i == 999
-		puts "Below:  #{below}"
-		puts "Resval: #{res_val}\n"
-	end
-	i = i+1%1000
-end
-
-puts "Solution: #{d}"
+puts "Smallest denominator having R(d) < 15499/94744 = #{i}"
