826d98fa05367adb3693cd3f069238f729fe5478
Ben <jrbennyboy11@gmail.com>
2016-01-24 20:31:30 -0500

    Updated problem 243 with benchmarking and status updates for every 10k calculations. Haven't been able to run to completion yet and computation time is well over 1 minute

--- problem243.rb
+++ problem243.rb
@@ -1,20 +1,43 @@
+=begin
+	Program by Ben Barnett
+	1/24/16
+	https://projecteuler.net/problem=243
+=end
+
+require 'benchmark'
+require 'rational'
+
 resilience_below = Rational(15499,94744)
 current_resilience = Rational(1)
-i = 1
+denominator = 12
 
-while(current_resilience >= resilience_below)
-	if i%1000 == 0 then puts "i: #{i}" end
+tenk_start = Time.now
+tenk_end = Time.now
+beginning_time = Time.now
 
-	j = 1
-	i += 1
-	num_resilient = 0
+while current_resilience >= resilience_below
+	i = 1
+	denominator += 1
+	num_resiliant = 0
 	
-	while(j < i)
-		if(Rational(j,i).denominator == i) then num_resilient += 1 end
-		j += 1
+	while i < denominator
+		if Rational(i,denominator).denominator == denominator
+			num_resiliant += 1
+		end
+		i += 1
 	end
-	
-	current_resilience = Rational(num_resilient,i-1)
+
+	current_resilience = Rational(num_resiliant, denominator-1)
+
+	if(denominator%10000 == 0)
+		tenk_end = Time.now
+		puts "Denominator: #{denominator}"
+		puts "#{denominator-10000} - #{denominator} time: About #{(tenk_end - tenk_start).ceil} seconds"
+		tenk_start = Time.now
+	end
 end
 
-puts "Smallest denominator having R(d) < 15499/94744 = #{i}"
+end_time = Time.now
+
+puts "R(#{denominator}) < 15499/94744"
+puts "Program Run Time: About #{(end_time - beginning_time).ceil} seconds"
