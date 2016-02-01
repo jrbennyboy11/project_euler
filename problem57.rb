0daa8c40ef6783910a4593eea14ca3bbfef7e51b
Ben Barnett <jrbennyboy11@gmail.com>
2016-02-01 15:13:38 -0500

    Added nth_sqrt_expansion function for possible reusability at a later point

--- problem57.rb
+++ problem57.rb
@@ -6,7 +6,41 @@
 	I solved this one by taking the information they gave me about the first 8 numerators and denominators and comparing them side by side in excel. I noticed that the numerator was going up by 2 times the denominator, and the denominator was going up by 1 tims the numerator, each time. This made writing a program extremely easy. 
 =end
 
+def nth_sqrt_expansion(num, expansion)
+	i = 1
+	i+=1 while i**2 < num
+	
+	numerator = (i-1)*num+1
+	denominator = num
+	i = expansion
+	
+	while i > 1
+		temp = numerator
+		numerator += num*denominator
+		denominator += temp
+		i -= 1
+	end
+	
+	#Note: Will return reduced fraction. Uncomment print statement to find irrational fraction
+=begin
+	puts "Numerator: #{numerator}"
+	puts "Denominator: #{denominator}"	
+=end
+	return Rational(numerator,denominator)
+end
+
 require 'benchmark'
+require 'rational'
+
+=begin
+#Code for testing the nth_sqrt_expansion function
+8.times do  |x|
+	puts nth_sqrt_expansion(2,x+1)
+end
+8.times do  |x|
+	puts nth_sqrt_expansion(5,x+1)
+end
+=end
 
 numerator = 3
 denominator = 2
